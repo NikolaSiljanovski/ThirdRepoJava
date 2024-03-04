91cc9d12ccd0ec0ea0dc0e583cb218923a6dfc55
NikolaSiljanovski <siljanovski0@gmail.com>
2024-03-04 11:36:35 +0100

    Add About.java

---
+++ src/main/java/About.java
@@ -0,0 +1,21 @@
+public class About {
+
+	public static void main(String[] args) {
+		
+		String NameSurname = "Nikola Siljanovski" ;
+		
+		Double Height = 192.5 ;
+		
+		char BloodType = 'B' ;
+		
+		int Weight = 85;
+		
+		
+		System.out.println(NameSurname + " " + Height + " " + "Blood Type = " + BloodType + " " + "Postiive " + " " + Weight + " KG "   );
+				
+
+				
+
+	}
+
+}
